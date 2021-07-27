b8d6bd4cd240e7415e5cee5c771fc03faa78f60e
martial-boniou <martial.boniou2021@campus-eni.fr>
2021-07-27 13:43:22 +0200

    module 5: entityManager

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,8 +27,41 @@ class SerieController extends AbstractController
     }
 
     #[Route('/create', name: 'create')]
-    public function create(): Response {
+    public function create(Request $request): Response {
         // todo: aller chercher les séries en BDD
+        return $this->render('serie/create.html.twig');
+    }
+
+    #[Route('/demo', name: 'em-demo')]
+    public function demo(EntityManagerInterface $em): Response {
+
+        // création d'une instance
+        $serie = new Serie;
+
+        // hydratation de toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('oeuo');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime('-1 year'));
+        $serie->setLastAirDate(new \DateTime('-6 month'));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('Cancelled');
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        $em->persist($serie);
+        $em->flush();
+
+        dump($serie);
+
+        $em->remove($serie);
+        $em->flush();
+
         return $this->render('serie/create.html.twig');
     }
 }
